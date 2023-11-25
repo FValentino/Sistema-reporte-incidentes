ffd6a6db931e46473e12e727d3c3fd60f967836d
Valentino Farias <fariasvalentino25@gmail.com>
2023-11-24 21:42:40 -0300

    creacion del proyecto

---
+++ reportes-incidentes/src/main/java/com/javaintermedio/api/reportesincidentes/ReportesIncidentesApplication.java
@@ -0,0 +1,13 @@
+package com.javaintermedio.api.reportesincidentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportesIncidentesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportesIncidentesApplication.class, args);
+	}
+
+}
---
+++ reportes-incidentes/src/test/java/com/javaintermedio/api/reportesincidentes/ReportesIncidentesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaintermedio.api.reportesincidentes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportesIncidentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
